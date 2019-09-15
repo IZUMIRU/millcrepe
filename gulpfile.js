a5bb73b6e10458b33572f422f12185bfa1371edc
AKIHIDEEgashira <shingashi0624@gmail.com>
2019-09-15 18:15:40 +0900

    feat/some files added and editted but not completed

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+// gulpプラグインを読み込みます
+const { src, dest, watch } = require("gulp");
+// Sassをコンパイルするプラグインを読み込みます
+const sass = require("gulp-sass");
+
+/**
+ * Sassをコンパイルするタスクです
+ */
+const compileSass = () =>
+  // style.scssファイルを取得
+  src("/sass/style.scss")
+    // Sassのコンパイルを実行
+    .pipe(
+      // コンパイル後のCSSを展開
+      sass({
+        outputStyle: "expanded"
+      })
+    )
+    // cssフォルダー以下に保存
+    .pipe(dest("css"));
+
+/**
+ * Sassファイルを監視し、変更があったらSassを変換します
+ */
+const watchSassFiles = () => watch("/sass/style.scss", compileSass);
+
+// npx gulpというコマンドを実行した時、watchSassFilesが実行されるようにします
+exports.default = watchSassFiles;
